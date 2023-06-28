46e8f1e3f73bf55f291fbf8946c752555f169794
Unagui19 <francorr199@gmail.com>
2023-06-28 20:12:44 -0300

    inciso a y b de punto2 terminados

--- Punto2/Empleados.cs
+++ Punto2/Empleados.cs
@@ -30,19 +30,95 @@ namespace EmpEspace
         public char Genero { get => genero; set => genero = value; }
         public DateTime FechaIngreso { get => fechaIngreso; set => fechaIngreso = value; }
         public double SueldoBasico { get => sueldoBasico; set => sueldoBasico = value; }
+
         internal Cargos Cargo { get => cargo; set => cargo = value; }
         
-    }
+        //metodos de la clase
+        public int antiguedad(DateTime fechaNac)
+        {
+            int anti;
 
-    
+            if (fechaNac.Day >= DateTime.Now.Day || fechaIngreso.Month >= DateTime.Now.Month)
+            {
+                    anti=DateTime.Now.Year-fechaIngreso.Year;
+            }
+            else
+            {
+                anti=DateTime.Now.Year-fechaIngreso.Year-1;
+            }
+            
+            return anti;
+        }
+
+        public int edad(DateTime fechaNac)
+        {
+            int edad;
+
+            if (fechaNac.Day >= DateTime.Now.Day || fechaNac.Month >= DateTime.Now.Month)
+            {
+                edad=DateTime.Now.Year-fechaNac.Year;
+            }
+            else
+            {
+                edad=DateTime.Now.Year-fechaNac.Year-1;
+            }
+            
+            return edad;
+        }
+
+        public void jubilacion(char genero)
+        {
+            int faltan;
+            
+            if (genero=='M')
+            {
+                faltan= 65 - edad(fechaNac);
+            }
+            else
+            {
+                faltan=60-edad(fechaNac);
+            }
+        }
+
+        public double Adicional(double sueldoBasico)
+        {
+            double adicional=0;
+            if (antiguedad(fechaIngreso)<20)
+            {
+                adicional+=sueldoBasico*antiguedad(fechaIngreso)/100;
+            }
+            else
+            {
+                adicional+=sueldoBasico*20/100+sueldoBasico*(antiguedad(fechaIngreso)-20)*25/100;
+            }
+
+            if ((cargo==Cargos.Especialista) || (cargo==Cargos.Ingeniero))
+            {
+                adicional+=adicional*50/100;
+            }
+
+            if (estadoCivil=='C')
+            {
+                adicional+=15000;
+            }
+            return adicional;
+        }
+        public double salario()
+        {
+            return sueldoBasico+Adicional(sueldoBasico);
+        }
+
+
+    }
 
 
     public enum Cargos
     {
         Auxiliar = 1,
         Administrativo = 2,
-        Especialista = 3,
-        Investigador = 4
+        Ingeniero = 3,
+        Especialista = 4,
+        Investigador = 5
     }
     
 }
